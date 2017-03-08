d09e76e87e370cf7405831279cf95404385d6646
Timo Verkroost <timo.verkroost@gmail.com>
2017-03-08 12:02:12 +0100

    Hide and show results

--- app/assets/js/modules/sections.js
+++ app/assets/js/modules/sections.js
@@ -1,7 +1,13 @@
 // Sections
 var sections = (function () {
     var hidden,
-        i;
+        i,
+        formInput,
+        searchFieldInput,
+        range,
+        buildYear,
+        haveResults,
+        noResults;
     return {
         hide: function () {
             // Hide other sections
@@ -13,15 +19,59 @@ var sections = (function () {
             }
         },
         saved: function () {
-            console.log("saved");
         },
         search: function () {
-            console.log("search");
-            requests.search();
+            haveResults = document.getElementById('haveSearchResults');
+            noResults = document.getElementById('noSearchResults');
+            // Get form with input values of user
+            formInput = document.getElementById('searchFields');
+            // To prevent duplicate eventListeners
+            formInput.removeEventListener("submit", getFormValues);
+            // On submit get values
+            formInput.addEventListener("submit", getFormValues);
+            // FormInput function
+            function getFormValues(event) {
+                // Prevent default submit
+                event.preventDefault();
+                // Get search input value and set in localStorage to use everywhere in the application
+                searchFieldInput = document.getElementById('searchField').value;
+                localStorage.setItem("fundaPlace", searchFieldInput);
+                // Get range input value and set in localStorage to use everywhere in the application
+                range = document.getElementById('range').value;
+                localStorage.setItem("fundaRange", range);
+                // Get buildYear input value and set in localStorage to use everywhere in the application
+                buildYear = document.getElementById('buildYear').value;
+                localStorage.setItem("fundaBuildYear", buildYear);
+                // Request search
+                requests.search();
+                // Hide no results and show results
+                noResults.classList.add("hide");
+                haveResults.classList.add("show");
+                // Remove old classes
+                noResults.classList.remove("show");
+                haveResults.classList.remove("hide");
+            }
+            // Loaded last search from storage
+            if (localStorage.getItem("fundaPlace") != null) {
+                requests.search();
+                // Hide no results and show results
+                noResults.classList.add("hide");
+                haveResults.classList.add("show");
+                // Remove old classes
+                noResults.classList.remove("show");
+                haveResults.classList.remove("hide");
+            } else {
+                // Hide results and show no results
+                noResults.classList.add("show");
+                haveResults.classList.add("hide");
+                // Remove old classes
+                noResults.classList.remove("hide");
+                haveResults.classList.remove("show");
+            }
         },
         suggested: function () {
-            console.log("suggested");
-        },
+            requests.getSuggestedHomes();
+        }
     };
 
 })();
